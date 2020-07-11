1eeba50035fa20be870b478cc6491dfc81e2dc6c
ngthotuan <ngthotuan@gmail.com>
2020-07-11 17:30:29 +0700

    update database

--- thietke/database-script.sql
+++ thietke/database-script.sql
@@ -1,50 +1,41 @@
-CREATE TABLE `conferences` (
-  `id` varchar(20) PRIMARY KEY,
-  `name` nvarchar(20),
-  `shortDescription` nvarchar(50),
-  `detailDescription` nvarchar(200),
-  `image` nvarchar(20),
-  `holdTime` datetime,
-  `conferenceTime` int
+DROP DATABASE ConferenceManagement;
+CREATE DATABASE ConferenceManagement;
+USE ConferenceManagement;
+CREATE TABLE `Conference` (
+                               `id` int PRIMARY KEY AUTO_INCREMENT,
+                               `placeId` int,
+                               `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+                               `shortDescription` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+                               `detailDescription` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+                               `image` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+                               `holdTime` datetime,
+                               `conferenceTime` long,
+                               `currentPerson` int DEFAULT 0
 );
 
-CREATE TABLE `venueLocations` (
-  `id` varchar(20) PRIMARY KEY,
-  `name` nvarchar(20),
-  `address` nvarchar(20),
-  `limitPerson` int
+CREATE TABLE `Place` (
+                         `id` int PRIMARY KEY AUTO_INCREMENT,
+                         `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+                         `address` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+                         `limitPerson` int
 );
 
-CREATE TABLE `conferenceVenueLocations` (
-  `conferenceId` varchar(20),
-  `holdTime` datetime,
-  `conferenceTime` int,
-  `venueLocationId` varchar(20),
-  PRIMARY KEY (`conferenceId`, `holdTime`, `conferenceTime`, `venueLocationId`)
+CREATE TABLE `User` (
+                         `username` varchar(20) PRIMARY KEY,
+                         `password` varchar(50),
+                         `name` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+                         `email` varchar(50),
+                         `isAdmin` bool
 );
 
-CREATE TABLE `accounts` (
-  `id` varchar(20) PRIMARY KEY,
-  `name` nvarchar(20),
-  `username` varchar(20),
-  `password` varchar(20),
-  `email` varchar(50),
-  `isAdmin` boolean
+CREATE TABLE `MeetingAccount` (
+                                   `userId` varchar(20),
+                                   `conferenceId` int,
+                                   PRIMARY KEY (`userId`, `conferenceId`)
 );
 
-CREATE TABLE `meetingAccounts` (
-  `accountId` varchar(20),
-  `conferenceId` varchar(20),
-  PRIMARY KEY (`accountId`, `conferenceId`)
-);
+ALTER TABLE `Conference` ADD FOREIGN KEY (`placeId`) REFERENCES `Place` (`id`);
 
-ALTER TABLE `conferenceVenueLocations` ADD FOREIGN KEY (`conferenceId`) REFERENCES `conferences` (`id`);
+ALTER TABLE `MeetingAccount` ADD FOREIGN KEY (`userId`) REFERENCES `User` (`username`);
 
-ALTER TABLE `conferenceVenueLocations` ADD FOREIGN KEY (`holdTime`) REFERENCES `conferences` (`holdTime`);
-
-ALTER TABLE `conferenceVenueLocations` ADD FOREIGN KEY (`conferenceTime`) REFERENCES `conferences` (`conferenceTime`);
-
-ALTER TABLE `conferenceVenueLocations` ADD FOREIGN KEY (`venueLocationId`) REFERENCES `venueLocations` (`id`);
-
-ALTER TABLE `meetingAccounts` ADD FOREIGN KEY (`accountId`) REFERENCES `accounts` (`id`);
-
+ALTER TABLE `MeetingAccount` ADD FOREIGN KEY (`conferenceId`) REFERENCES `Conference` (`id`);
